9bf0ad4bccc28683b0919151799e39f86151f5b3
ichcamile <camilesantana21@gmail.com>
2023-11-12 00:49:31 -0300

    add more components> main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,19 +9,76 @@ import Posted from './posted/Posted';
 function Main() {
   return (
     <>
-      <Uppersite/>
-        <Tweet
+      <Uppersite />
+      <Tweet
         girlfriend={girlfriend}
         tweetWrite="Olá, esse é o meu primeiro tweet"
-        whoReply="Everyone can reply"/>
-        <hr/>
+        whoReply="Everyone can reply" />
+      <hr />
       <Posted
-      name={"Gabriel Lindo Augusto"}
-      username={"Camile"}
-      nickname={"@ichcamile"}
-      time={"10"}
-      
+        name={"Gabriel Lindo Augusto"}
+        username={"Camile"}
+        nickname={"@ichcamile"}
+        time={"10"}
+
       />
+      <Posted
+        name={"Sophia Grace"}
+        username={"Daniel"}
+        nickname={"@dannyboy"}
+        time={"25m"}
+        contentTweet={"Trying out new recipes in the kitchen and loving every moment of it! 🍲👩‍🍳 #CookingAdventures #Foodie"}
+      />
+
+      <Posted
+        name={"Lily Johnson"}
+        username={"Ryan"}
+        nickname={"@ryanj"}
+        time={"45min"}
+        contentTweet={"Embracing the beauty of nature on a morning hike. 🌲🏞️ Feeling refreshed and alive! #NatureLover #MorningHike"}
+      />
+
+      <Posted
+        name={"Elena Rodriguez"}
+        username={"Carlos"}
+        nickname={"@carlitos"}
+        time={"1h"}
+        contentTweet={"Creating art is my therapy. 🎨✨ Exploring new techniques and styles. #ArtisticJourney #Creativity"}
+      />
+
+      <Posted
+        name={"Alex Turner"}
+        username={"Emma"}
+        nickname={"@emma_fitness"}
+        time={"2h"}
+        contentTweet={"Spending the day at the beach, soaking up the sun and enjoying the ocean breeze. 🏖️☀️ #BeachDay #Relaxation"}
+      />
+
+      <Posted
+        name={"Olivia Smith"}
+        username={"Michael"}
+        nickname={"@mike_coffee"}
+        time={"3h"}
+        contentTweet={"Exploring the city streets and finding hidden gems. 🌆🗺️ #CityExploration #AdventureTime"}
+      />
+
+      <Posted
+        name={"Nathan White"}
+        username={"Sophie"}
+        nickname={"@sophie_tweets"}
+        time={"4h"}
+        contentTweet={"Chasing sunsets and capturing the magical moments. 🌅📷 #SunsetChaser #PhotographyLover"}
+      />
+
+      <Posted
+        name={"Isaac Brown"}
+        username={"Aria"}
+        nickname={"@ariasinger"}
+        time={"5h"}
+        contentTweet={"Reflecting on life's journey and counting my blessings. 🙏✨ #Gratitude #LifeReflections"}
+      />
+
+
     </>
   )
 }
--- src/components/posted/Posted.jsx
+++ src/components/posted/Posted.jsx
@@ -1,33 +1,60 @@
 import React from 'react'
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { AiOutlineHeart, AiFillHeart, AiOutlineRetweet } from 'react-icons/ai';
 import girlfriend from "../../assets/profile-pic.png"
 import "../posted/posted.css"
 import { PiDotBold } from 'react-icons/pi';
+import { LuMessageSquare } from 'react-icons/lu';
+import { FiUpload } from 'react-icons/fi';
 
 
-function Posted({ name, username, nickname, time }) {
+
+function Posted({ name, username, nickname, time, contentTweet }) {
   return (
     <div className='posted'>
+
       <header className='headerPosted'>
-        <div><AiFillHeart /></div>
+        <AiFillHeart />
         <p>{name} liked</p>
       </header>
-      <div className='teste'>
+
+      <div className='content'>
         <aside>
           <img className='profilePic' src={girlfriend} alt='profilePic' />
         </aside>
-        <div>
-          <div className='secondHeader'>
+
+
+
+      </div>
+
+      <div className='main'>
+
+        <div className='secondHeader'>
             <h2>{username}</h2>
             <p>{nickname}</p>
             <div><PiDotBold /></div>
             <p> {time} min</p>
-          </div>
         </div>
-        <div>
 
+        <div>
+            <p>{contentTweet}</p>
         </div>
+        
+        <hr/>
+
+
+        <div className='icons'>
+          <div><LuMessageSquare/></div>
+          <div><AiOutlineRetweet/></div>
+          <div><AiFillHeart/></div>
+          <div><FiUpload/></div>
       </div>
+      </div>
+
+
+
+      
+
+
 
     </div>
   )
